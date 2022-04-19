e2c1767398d0232040db791f319701709448020f
heymoko <gozila__adm@naver.com>
2022-04-19 18:39:26 +0900

    git-repo-app

--- app/src/main/java/com/project/githubrepositoryapp/MainActivity.kt
+++ app/src/main/java/com/project/githubrepositoryapp/MainActivity.kt
@@ -1,11 +1,62 @@
 package com.project.githubrepositoryapp
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.browser.customtabs.CustomTabsIntent
+import com.project.githubrepositoryapp.databinding.ActivityMainBinding
+import kotlinx.coroutines.*
+import kotlin.coroutines.CoroutineContext
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), CoroutineScope {
+
+    private lateinit var binding: ActivityMainBinding
+
+    val job: Job = Job()
+
+    override val coroutineContext: CoroutineContext
+        get() = Dispatchers.Main + job
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initViews()
+    }
+
+    private fun initViews() = with(binding) {
+        loginButton.setOnClickListener {
+            loginGithub()
+        }
+    }
+
+    private fun loginGithub() {
+        val loginUri = Uri.Builder().scheme("https").authority("github.com")
+            .appendPath("login")
+            .appendPath("oauth")
+            .appendPath("authorize")
+            .appendQueryParameter("client_id", BuildConfig.GITHUB_CLIENT_ID)
+            .build()
+
+        CustomTabsIntent.Builder().build().also {
+            it.launchUrl(this, loginUri)
+        }
+    }
+
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+
+        intent?.data?.getQueryParameter("code")?.let {
+            launch(coroutineContext) {
+                getAccessToken(it)
+            }
+        }
+    }
+
+    private suspend fun getAccessToken(code: String) = withContext(Dispatchers.IO) {
+        val response
     }
 }
